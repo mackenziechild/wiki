c290ff5270d8f9ae8655845ae6cf51a15d75ff71
Mackenzie Child <mackenzie.child@gmail.com>
2014-11-01 17:06:43 -0600

    Installed simple_form and created article show pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show]
 	def index
+	end
+
+	def show
 	end
 
 	def new
@@ -16,6 +20,10 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params
 		params.require(:article).permit(:title, :content)
