4f668e33c9bb5ce7a40022c5d323892f66c3c42c
Mackenzie Child <mackenzie.child@gmail.com>
2014-11-02 08:03:59 -0700

    Add Categories to Articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101232113) do
+ActiveRecord::Schema.define(version: 20141102144114) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,9 +19,16 @@ ActiveRecord::Schema.define(version: 20141101232113) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
